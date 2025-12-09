1578e24a372ed521620b0bd26b5c16182209fa9e
uvmsci <uvmsci@gmail.com>
2025-12-09 15:48:01 +0100

    build: updates url for central sonatype
    With the shutdown of the old OSSRH new versions are no longer available
    through the old search URL.
    
    The compatibility API that they created doesn't work exactly the same,
    i.e. it doesn't seem to return latestVersion in the response anymore.

--- getVersion.sh
+++ getVersion.sh
@@ -3,5 +3,5 @@
 group="${1:-fish.focus.uvms.docker}"
 artifact="${2:-uvms-docker-wildfly-unionvms}"
 
-curl -s https://search.maven.org/solrsearch/select\?q\=g:$group+AND+a:$artifact | grep -Po 'latestVersion.:.\K[^"]*'
+curl -s https://central.sonatype.com/solrsearch/select\?q\=g:$group+AND+a:$artifact\&sort\=v+desc\&rows\=1 | grep -Po 'v.:.\K[^"]*'
 
